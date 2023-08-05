68b1c661f13013c07134219a0622a1523b54f6cb
luanfigueredodesousa <luanfigueredo22@gmail.com>
2023-08-05 16:52:12 -0300

    unit test

--- src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
+++ src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
@@ -1,2 +1,28 @@
-package br.ce.wcaquino.servicos;public class LocacaoServiceTest {
+package br.ce.wcaquino.servicos;
+
+import br.ce.wcaquino.entidades.Filme;
+import br.ce.wcaquino.entidades.Locacao;
+import br.ce.wcaquino.entidades.Usuario;
+import br.ce.wcaquino.utils.DataUtils;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+import java.util.Date;
+
+public class LocacaoServiceTest {
+    @Test
+    public void teste(){
+        //cenario
+        LocacaoService service = new LocacaoService();
+        Usuario usuario = new Usuario("Usuário 1");
+        Filme filme = new Filme("Filme 1", 2, 5.0);
+
+        //acao
+        Locacao locacao = service.alugarFilme(usuario, filme);
+
+        //verificacao
+        Assert.assertTrue(locacao.getValor() == 5.0);
+        Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
+        Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
+    }
 }
